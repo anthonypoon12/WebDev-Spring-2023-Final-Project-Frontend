849fc8b73285afec427af88168d4befb00182f1d
anthonypoon12 <anthony@poons.us>
2023-05-24 19:19:26 -0400

    added link to employee from task

--- src/components/views/TaskView.js
+++ src/components/views/TaskView.js
@@ -5,7 +5,7 @@ const TaskView = (props) => {
   return (
     <div>
       <h1>{task.title}</h1>
-      {task.employee ? <h3>{task.employee.firstname + " " + task.employee.lastname}</h3>: <h3>staff</h3>}
+      {task.employee ? <Link to={`/employee/${task.employeeId}`}><h3>{task.employee.firstname + " " + task.employee.lastname}</h3></Link>: <h3>staff</h3>}
       <Link to={`/edittask/${task.id}`}>Edit task information</Link>
       <br/>
       <Link to={`/tasks`}>View all tasks</Link>
