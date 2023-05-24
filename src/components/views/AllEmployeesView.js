2abe8c69300bbb41db07f02b4c655c8d0a79b95c
anthonypoon12 <anthony@poons.us>
2023-05-24 19:30:47 -0400

    if no employees add employees btn shows

--- src/components/views/AllEmployeesView.js
+++ src/components/views/AllEmployeesView.js
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 
 const AllEmployeesView = (props) => {
   if (!props.allEmployees.length) {
-    return <div>There are no employees.</div>;
+    return (
+      <div>
+        <div>There are no employees.</div>
+        <Link to={`/newemployee`}>
+          <button>Add New Employee</button>
+        </Link>
+      </div>
+    );
   }
 
   return (
